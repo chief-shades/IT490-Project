1b4b98630e6f8ef991e21b961f9bb7c5ae225c4e
chief <55118970+chief-shades@users.noreply.github.com>
2019-12-07 23:28:29 +0000

    Create scp.sh
    file used for scp tranfers 192.168.86.23 is mq server with appropriate dir

---
+++ scp.sh
@@ -0,0 +1,11 @@
+scp -r /home/IT490-Project/ebay_frontEnd.html 192.168.86.23/home/scptransfers/appserver/
+echo "sent to MQ-Server"
+
+scp -r /home/IT490-Project/login_register_forms.html 192.168.86.23/home/scptransfers/appserver/
+echo "sent to MQ-Server"
+
+scp -r /home/IT490-Project/readme.me 192.168.86.23/home/scptransfers/appserver/
+echo "sent to MQ-Server"
+
+scp -r /home/IT490-Project/scripts 192.168.86.23/home/scptransfers/appserver/
+echo "sent to MQ-Server"
